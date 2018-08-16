9d97f24bbe7cc54e91235201b8ba722f68d1b5a5
Muhun Kim <iam@muhun.kim>
2018-08-17 00:18:18 +0900

    Add test line

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ from commitTelegramBot import CommitTelegramBot
 
 if __name__ == '__main__':
 	bot = CommitTelegramBot(argv[1], argv[2], argv[3], argv[4])
+	print("today's commit count total : %s" % bot.count_total())
 	bot.handler()
